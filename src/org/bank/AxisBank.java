86dbe8178764c4f41f0e951f90b2fa2386370395
EsaiyarasanS <esaiyarasan.aisect@gmail.com>
2023-06-06 13:48:31 +0530

    my task

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+@Override
+public void deposit() {
+	// TODO Auto-generated method stub
+	System.out.println("deposit : 14%");
+	
+}
+public static void main(String[] args) {
+	AxisBank a=new AxisBank();
+	a.deposit();
+}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class BankInfo  {
+private void saving() {
+	// TODO Auto-generated method stub
+System.out.println("saving : 10%");
+}
+private void fixed() {
+	// TODO Auto-generated method stub
+System.out.println("fixed : 18%");
+}
+public void deposit() {
+	// TODO Auto-generated method stub
+System.out.println("deposit12%");
+}
+
+}
